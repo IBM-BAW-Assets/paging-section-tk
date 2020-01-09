5540febb3be2fc9c63645f577ed9d64dc3205d7f
Leonard Blunt <leonard.blunt@gmail.com>
2020-01-09 09:51:06 +0800

    Add files via upload

---
+++ EXAMPLE_DATA/EG_CITY_CODE_sample.sql
@@ -0,0 +1,13 @@
+INSERT INTO PROTELDBNEW.EG_CITY_CODE (CITY_NM,CD) VALUES 
+('Singapore','SIN')
+,('Jakarta','JKT')
+,('Manila','MNL')
+,('Bangkok','BKK')
+,('Kuala Lumpur','KUL')
+,('Tokyo','TYO')
+,('Seoul','SEL')
+,('Hanoi','HAN')
+,('Taipei','TPE')
+,('Mumbai','BOM')
+,('Phnom Penh','PNH')
+;
