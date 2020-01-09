2a12eaaf921d2b6bf1770bdcee3b9f57dd853b22
Leonard Blunt <leonard.blunt@gmail.com>
2020-01-09 08:51:48 +0800

    Create default-SortConfigList.js

---
+++ snippets/default-SortConfigList.js
@@ -0,0 +1,8 @@
+var autoObject = new tw.object.listOf.SortConfig();
+autoObject[0] = new tw.object.SortConfig();
+autoObject[0].viewName = "Example1";
+autoObject[0].displayName = "Date of Purchase";
+autoObject[0].dbColumnName = "PURCHASE_DT";
+autoObject[0].sortOrder = "ASC";
+autoObject[0].sortAscending = true;
+autoObject
